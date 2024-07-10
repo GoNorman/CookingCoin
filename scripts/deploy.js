b2e920fed2a408862c9afb0071e2176ff531bfb3
GoNorman <dr.cheif-afanasyev@yandex.ru>
2024-07-10 11:24:54 +0200

    Final work with deploy. First contract with address: 0x5FbDB2315678afecb367f032d93F642f64180aa3 complited on Polygon BlockChain

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,19 @@
+const hre = require("hardhat");
+
 async function main() {
-    const Counter = await ethers.getContractFactory("Counter");
-    
-    const counter = await Counter.deploy("Counter");   
-    console.log("Contract deployed to address:", counter.address);
- }
- 
- main()
-   .then(() => process.exit(0))
-   .catch(error => {
-     console.error(error);
-     process.exit(1);
-   });
+    const Counter = await hre.ethers.getContractFactory("Counter");
+    const counter = await Counter.deploy();
+
+    // Waiting for the deployment transaction to complete
+    await counter.deploymentTransaction().wait();
+
+    // We get the contract address from 'receipt'
+    const receipt = await hre.ethers.provider.getTransactionReceipt(counter.deploymentTransaction().hash);
+
+    console.log("Counter deployed to:", receipt.contractAddress);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
